28830df2dd9b8bc3c504c980cd9508300640c63d
App Academy Student <student@appacademy.io>
2016-12-08 16:59:07 -0800

    Added band, album, track tables and models

---
+++ Music/db/migrate/20161209005143_create_albums.rb
@@ -0,0 +1,11 @@
+class CreateAlbums < ActiveRecord::Migration
+  def change
+    create_table :albums do |t|
+      t.string :title, null: false
+      t.integer :band_id, null: false
+      t.timestamps null: false
+    end
+    add_index :albums, :title
+    add_index :albums, :band_id 
+  end
+end
